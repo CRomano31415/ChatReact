e06823d25aa36a4ec82dd8f6dcca619aaf6cf5de
Claudia Romano <cromano31415@gmail.com>
2018-02-20 16:13:14 -0600

    working on delete messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -42,6 +42,35 @@ class MessageList extends Component {
 		this.setState({ newMessages: ''});
 	}
 
+	// updateMessage(e) {
+	//   	e.preventDefault();
+	// 	this.msgRef.child(put key here).update({
+	// 		content: this.state.newMessages,
+	// 		roomId: this.props.activeRoom,
+	// 		sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+	// 		username: this.props.activeUser
+	// 	});
+	// 	this.setState({ newMessages: ''});
+	// }
+
+	deleteMsg(e) {
+	  	e.preventDefault();
+	  	let activeRoomKey;
+	  	let activeMsgKey;
+	  	//this will get me the active room's key
+	  	this.state.rooms.forEach((element)=>{
+		  	if(element.name === this.props.activeRoom){
+	  			activeRoomKey = element.key;
+	  			return;
+		  	}
+	  	});
+	  	//using the key, remove the item from db
+		this.roomsRef.child(activeRoomKey).remove(); 
+		this.setState({ newRooms: '' });
+		(this.state.rooms.length > 0) ? this.setState({ createNewRoom: false}) : this.setState({ createNewRoom: true});
+	}
+
+
 	handleChange(e) {
 	  	this.setState({ newMessages: e.target.value });
 	}
@@ -57,6 +86,7 @@ class MessageList extends Component {
 				    	 this.displayMessages().map( (msg) =>
 							<li key={ msg.key } roomid={ msg.roomId } sentat={ msg.sentAt } username={ msg.username } >   
 								{msg.username}: { msg.content }
+								<input type="checkbox" key={ msg.key }/>
 							</li>
 					 	)
 					 }
@@ -68,6 +98,7 @@ class MessageList extends Component {
 						 {this.props.activeUser + " : "}
 						 <input className='MsgText' type='text' value={this.state.newMessages} onChange={ (e) => this.handleChange(e) } />
 						 <button type='submit' value='Enter' onClick={(e) => this.enterMessage(e)}>Enter </button> 
+						 <button type='submit' value='Delete' onClick={(e) => this.deleteMsg(e)}>Delete Selected Messages </button> 
 						 </form>
 					</div>
 				} { this.props.activeRoom === null && "Select a room to view messages"}
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -9,7 +9,8 @@ class RoomList extends Component {
 		this.state = {
 			rooms: [],
 			newRooms: '',
-			createNewRoom: false
+			createNewRoom: false,
+			editRoomName: false
 		}
 		this.roomsRef = this.props.firebase.database().ref('rooms');
 	}
@@ -18,7 +19,7 @@ class RoomList extends Component {
 			this.roomsRef.on('child_added', snapshot => {
 			const room = snapshot.val();
 			room.key = snapshot.key;
-			this.setState({ rooms: this.state.rooms.concat( room ) });
+			this.setState({ rooms: this.state.rooms.concat( room ) }, () => {console.log(this.state.rooms)});
 		});
 	}
 
@@ -48,6 +49,23 @@ class RoomList extends Component {
 	  	});
 	  	//using the key, remove the item from db
 		this.roomsRef.child(activeRoomKey).remove(); 
+		this.setState({ newRooms: '' });
+		(this.state.rooms.length > 0) ? this.setState({ createNewRoom: false}) : this.setState({ createNewRoom: true});
+	}
+
+	editRoom(e) {
+	  	e.preventDefault();
+		this.setState({editRoomName: true});
+	  	let activeRoomKey;
+	  	//this will get me the active room's key
+	  	this.state.rooms.forEach((element)=>{
+		  	if(element.name === this.props.activeRoom){
+	  			activeRoomKey = element.key;
+	  			return;
+		  	}
+	  	});
+	  	//using the key, update the item from db
+		this.roomsRef.child(activeRoomKey).update({ name: this.state.newRooms }); 
 		this.setState({ newRooms: '' });
 		(this.state.rooms.length > 0) ? this.setState({ createNewRoom: false}) : this.setState({ createNewRoom: true});
 	}
@@ -88,6 +106,14 @@ class RoomList extends Component {
 						</li>
 				 	)}
 			 	<button type='submit' id='Delete' onClick={(e) => this.deleteRoom(e)}>Delete a Room</button>
+			 	<button type='submit' id='Edit' onClick={(e) => this.editRoom(e)}>Edit a Room's Name</button>
+				{ this.state.editRoomName &&
+				<div className="ChatRoom">
+					 Enter a new room name for { this.props.activeRoom }
+					 <input className='RoomTxt' type='text' value={ this.state.newRooms } onChange={ (e) => this.handleChange(e) } />
+					 <button type='submit' value='Create room' onClick={(e) => this.editRoom(e)}> Update Name </button> 
+				</div>
+				}
 				 </ul>
 				</div>
 			</div>
